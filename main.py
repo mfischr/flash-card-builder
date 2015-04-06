861b590f6144bf045c741352c39b17918ddcba81
Michael Fischer <fischer.mt@gmail.com>
2015-04-06 15:40:52 +0800

    Refactor sentences code, write unit tests

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 import os
+import logging
 import json
-import requests
 import ankiutils
 from ccedict import Ccedict
+from sentences import SentenceDownloader
 
 # class Sentence():
 #     def __init__(self, chinese, english):
@@ -12,12 +13,18 @@ from ccedict import Ccedict
 #         return
 
 
-def loadConfig(argv):
+def _load_config(argv):
+    """
     # Should've passed the name of a text file in which to count words
+    :rtype : (dict) config dictionary
+    """
     if (len(argv) == 2):
         real_path = os.path.expanduser(argv[1])
         with open(real_path, "r") as configFile:
             config = json.load(configFile)
+            logging.basicConfig(filename = config['log_file'], level = logging.DEBUG)
+            logging.info("Loaded config file from %s", config['log_file'])
+
             config['cedict_path'] = os.path.expanduser(config['cedict_path'])
 
             return config
@@ -26,47 +33,27 @@ def loadConfig(argv):
 
 
 
-def get_examples(word):
-    """Downloads example sentences for a given word from Bing"""
-    import urllib.parse
-    from lxml import html
 
-    base_url = "http://cn.bing.com/dict/search"
-    query_string = urllib.parse.urlencode({ "q": word })
-
-    response = requests.get(base_url + "?" + query_string)
-    dom = html.fromstring(response.content)
-
-    dom_sentences = dom.xpath('//*[@class="se_li"]')
-
-    sentences = []
-
-    for s in list(dom_sentences):
-        english = "".join(s.xpath('.//*[@class="sen_en"]//text()'))
-        chinese = "".join(s.xpath('.//*[@class="sen_cn"]//text()'))
-        
-        sentences.append({ "english": english, "chinese": chinese})
-
-    return sentences
 
 
 
 if __name__ == "__main__":
 
-    config = loadConfig(sys.argv)
+    config = _load_config(sys.argv)
 
     dict = Ccedict(config['cedict_path'])
 
     print("Loaded cedict, {0} words".format(len(dict.words.keys())))
 
     # Create a fake card
-    word_key = list(dict.words.keys())[100]
-    word = dict.words[word_key]
+    words = config['temp_word_list'].split(";")
 
-    cards = [{"id": "5", "front": word['hanzi'], "back": word['definition'] }]
-    file_name = ankiutils.write_deck_for_import("test", cards)
+    
 
-    print("Wrote cards to {0}".format(file_name))
-
-# with open("sentences.json", "w") as out_file:
-#     json.dump(examples, out_file)
+    # for word in words:
+    #     entry = dict.words[word]
+    #     cards.append({'id': word, 'front': })
+    #
+    # file_name = ankiutils.write_deck_for_import("test", cards)
+    #
+    # print("Wrote cards to {0}".format(file_name))
---
+++ sentences.py
@@ -0,0 +1,115 @@
+# Gets example sentences from the internet
+import os
+import shutil
+import unittest
+import logging
+import json
+import codecs
+import requests
+import urllib.parse
+
+def _ensure_dir(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+def _clear_dir(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+
+def _make_valid_filename(str):
+    """
+    From http://stackoverflow.com/questions/295135/turn-a-string-into-a-valid-filename-in-python
+    """
+    return "".join((x if x.isalnum() else "_") for x in str)
+
+
+class SentenceDownloader:
+    _BING_SENTENCES_BASE_URL = "http://cn.bing.com/dict/search"
+
+    def __init__(self, sentence_cache_folder):
+        self.sentence_cache_folder = sentence_cache_folder
+        self.download_count = 0
+        _ensure_dir(self.sentence_cache_folder)
+
+
+    def _download_bing_sentences(self, word):
+        """
+        Downloads example sentences for the given word, getting it from the
+        cache if possible.  Returns the content of the html page.
+        :param word: hanzi of word to search for
+        :return:
+        """
+
+        query_string = urllib.parse.urlencode({ "q": word })
+        url = SentenceDownloader._BING_SENTENCES_BASE_URL + "?" + query_string
+
+        cache_file = os.path.join(self.sentence_cache_folder, _make_valid_filename(url) + ".txt")
+
+        if not os.path.exists(cache_file):
+            logging.info("Requesting content from '%s'", url)
+
+            response = requests.get(url)
+            self.download_count += 1
+            response_as_text = response.content.decode(encoding = response.encoding)
+
+            with codecs.open(cache_file, 'w', 'utf-8') as file:
+                file.write(response_as_text)
+
+            logging.info("Saved content to '%s'", cache_file)
+            return response_as_text
+
+        else:
+            logging.info("Loading cached content from '%s'", cache_file)
+
+            with codecs.open(cache_file, 'r', 'utf-8') as file:
+                return file.read()
+
+
+
+    def get_sentences(self, word):
+        """Downloads example sentences for a given word from Bing"""
+        from lxml import html
+
+        content = self._download_bing_sentences(word)
+        dom = html.fromstring(content)
+
+        dom_sentences = dom.xpath('//*[@class="se_li"]')
+
+        logging.debug("  get_sentences: [%s] parsing %d sentences", word, len(dom_sentences))
+
+        sentences = []
+
+        for s in list(dom_sentences):
+            english = "".join(s.xpath('.//*[@class="sen_en"]//text()'))
+            chinese = "".join(s.xpath('.//*[@class="sen_cn"]//text()'))
+
+            sentences.append({ "english": english, "chinese": chinese})
+
+        return sentences
+
+
+
+class UnitTests(unittest.TestCase):
+    _TEMP_SENTENCE_CACHE_FOLDER = os.path.expandvars("$Temp\\sentences_py_unittest_cache")
+
+    def setUp(self):
+        _clear_dir(UnitTests._TEMP_SENTENCE_CACHE_FOLDER)
+
+    def tearDown(self):
+        pass
+
+    def test_clean_download(self):
+        """Should download sentences for a given word, and use cache"""
+        me = SentenceDownloader(UnitTests._TEMP_SENTENCE_CACHE_FOLDER)
+
+        test_word = u"\u4fdd\u62a4"
+        sentences = me.get_sentences(test_word) # bao3hu4, protect
+
+        self.assertEqual(len(sentences), 10, "sentence count")
+        self.assertTrue(sentences[0]['chinese'].find(test_word) >= 0, "downloaded sentence contains word")
+
+        sentences2 = me.get_sentences(test_word) # same thing
+        self.assertEqual(1, me.download_count, "download count")
+        self.assertEqual(json.dumps(sentences), json.dumps(sentences2), "sentences downloaded should be identical")
+
+
